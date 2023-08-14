36119b97c2375efd6a30d9e2627dac955b0b3086
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-08-14 18:36:58 -0400

    now fetching timeline post from database and using real data for post component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import Post from "../post/Post"
 import Share from "../share/Share"
 import "./feed.scss"
-import { Posts } from "../../dummyData"
+import axios from "axios";
 
 const Feed = () => {
+  const [ posts, setPosts ] = useState( [] );
+
+  useEffect( () => {
+    const fetch = async () => {
+      const res = await axios.get( "http://localhost:8800/api/posts/timeline/64d684a6f994b9d75d9e4d1d" )
+      // set posts data from most recent post to oldest
+      setPosts( res.data.sort( ( a, b ) => b.createdAt.localeCompare( a.createdAt ) ) )
+    }
+    return () => {
+      fetch()
+    }
+  }, [] )
+
+
   return (
     <div className="feed">
       <div className="wrapper">
         <Share />
-        { Posts.map( ( post ) => (
-          <Post key={ post.id } post={ post } />
+        { posts.map( ( post ) => (
+          <Post key={ post._id } post={ post } />
         ) ) }
       </div>
     </div>
