1eaa0ec894dbd13ed6d6930c435ae92efc2fada0
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-08-16 19:56:45 -0400

    users can now create a post (without img)

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,25 +1,49 @@
 import "./share.scss"
 import noavatar from "../../assets/person/noavatar.png"
 import { PermMedia, Label, Room, EmojiEmotions } from "@mui/icons-material"
-import { useContext } from "react"
+import { useContext, useRef, useState } from "react"
 import { AuthContext } from "../../context/AuthContext"
+import axios from "axios"
 
 const Share = () => {
     const { user } = useContext( AuthContext )
+    const desc = useRef()
+    const [ file, setFile ] = useState( null )
+
+    const handleSubmit = async ( e ) => {
+        e.preventDefault()
+        const newPost = {
+            userId: user._id,
+            desc: desc.current.value,
+            img: file
+        }
+        try {
+            await axios.post( "http://localhost:8800/api/posts", newPost )
+        } catch ( error ) {
+            console.log( error )
+        }
+    }
+
     return (
         <div className='share'>
             <div className="wrapper">
                 <div className="top">
                     <img className="profileImg" src={ user.profilePicture ? user.profilePicture : noavatar } alt="" />
-                    <input type="text" placeholder="What's on your mind?" className="input" />
+                    <input
+                        type="text"
+                        placeholder={ `What's on your mind ${user.firstName.charAt( 0 ).toUpperCase() + user.firstName.slice( 1 )}?` }
+                        className="input"
+                        ref={ desc }
+                    />
                 </div>
                 <hr className="shareHR" />
-                <div className="bottom">
+                <form className="bottom" onSubmit={ handleSubmit }>
                     <div className="options">
-                        <div className="option">
+                        <label htmlFor="file" className="option">
                             <PermMedia className="icon" htmlColor="tomato" />
                             <span className="text">Photo/Video</span>
-                        </div>
+                            <input type="file" name="file" id="file" accept=".png,.jpeg,.jpg" onChange={ e => setFile( e.target.files[ 0 ] ) } style={ { display: "none" } } />
+                        </label>
                         <div className="option" >
                             <Label className="icon" htmlColor="indigo" />
                             <span className="text">Tag</span>
@@ -33,8 +57,8 @@ const Share = () => {
                             <span className="text">Feelings</span>
                         </div>
                     </div>
-                    <button className="shareButton">Share</button>
-                </div>
+                    <button className="shareButton" type="submit">Share</button>
+                </form>
             </div>
         </div>
     )
